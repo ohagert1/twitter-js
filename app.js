12c00c01c2b2c90e523c5e96730f13824cb89f4c
Maxwell Legocki <mdlegocki@gmail.com>
2017-11-07 10:55:09 -0500

    started using nunjucks

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+const router = require('./routes/index');
 const app = express();
 const port = 3000;
-const router = require('./routes/index');
+nunjucks.configure('../views');
+const views = nunjucks.render('index.html', (err, output) => {
+  console.log(output);
+});
 
 app.listen(port, () => {
   console.log('server listening');
 });
 
 app.use(router);
+
