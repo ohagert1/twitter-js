572b17c878d832123a4e184eca033153bc58f9b8
Maxwell Legocki <mdlegocki@gmail.com>
2017-11-07 10:40:28 -0500

    abstracted routes, added request logging

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path);
+  next();
+});
+
 router.get('/', (req, res) => {
   res.send('WELCOME');
 });
 
-module.exports = router;
 
-//test
+module.exports = router;
